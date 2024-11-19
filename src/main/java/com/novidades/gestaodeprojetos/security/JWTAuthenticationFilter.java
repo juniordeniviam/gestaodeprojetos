624a36bdded5c7104ed47ce1cbbbfaca1bd03493
José Carlos <suportejr@outlook.com>
2024-11-19 16:32:58 -0300

    feat: criando a classe JWTAuthenticationFilter

---
+++ src/main/java/com/novidades/gestaodeprojetos/security/JWTAuthenticationFilter.java
@@ -0,0 +1,89 @@
+package com.novidades.gestaodeprojetos.security;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Optional;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JWTAuthenticationFilter extends OncePerRequestFilter{
+	
+	@Autowired
+	private JWTService jwtService;
+	
+	@Autowired
+	private CustomUserDetailsService customUserDetailsService;
+	
+	// Método principal onde toda requisição bate antes de chegar no nosso endpoint.
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) 	throws ServletException, IOException {
+		
+		// pego o token de dentro da requisição
+		String token = obterToken(request);
+		
+		// Pego o id do usuário que está dentro do token
+		Optional<Long> id = jwtService.obterIdDoUsuario(token);
+		
+		// Se não achou o id, é porque o usuário não mandou o token correto.
+		if(!id.isPresent()) {
+			// Retorna resposta 401 caso o token seja inválido
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			response.getWriter().write("Token inválido ou ausente!");
+			return;
+		}
+		
+		// Pego o usuario dono do token pelo seu id.
+		UserDetails usuario = customUserDetailsService.obterUsuarioPorId(id.get());
+		
+		// Se o usuário não for encontrado
+		if (usuario == null) {
+			// Retorna resposta 401
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			response.getWriter().write("Usuário não encontrado!");
+			return;
+		}
+		
+		// Neste trecho verificamos se o usuário está autenticado ou não.
+		// Neste trecho tambem poderia ser validado as permissões -> usuario.getAuthorities()
+		UsernamePasswordAuthenticationToken autenticacao = new UsernamePasswordAuthenticationToken(usuario, null, Collections.emptyList());
+		
+		// Mudando a autenticação para a própria requisição - Adiciona os detalhes da requisição
+		autenticacao.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+		
+		// Repasso a autenticação para o contexto do security.
+		// A partir de agora o spring toma conta de tudo para mim :D
+		SecurityContextHolder.getContext().setAuthentication(autenticacao);
+		
+		// Passa a requisição para o próximo filtro
+		filterChain.doFilter(request, response);
+		
+	}
+	
+	private String obterToken(HttpServletRequest request) {
+		
+		String token = request.getHeader("Authorization");
+		
+	    // Verifica se o token existe e começa com o prefixo "Bearer "
+	    if (StringUtils.hasText(token) && token.startsWith("Bearer ")) {
+	        return token.substring(7); // Remove o prefixo "Bearer "
+	    }
+
+	    return null; // Retorna null caso o token não esteja presente ou seja inválido
+	}
+
+
+
+}
